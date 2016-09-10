ed48f6c108caa28645eabac494412f8c719fa54d
Shreejit K <shreejit.kakkassery@in.tesco.com>
2016-09-10 16:25:02 +0530

    [Shreejit][Added test cases to handle negative scenario for converstion]

--- src/main/java/com/myproject/exercise/utils/RoundOfOrder.java
+++ src/main/java/com/myproject/exercise/utils/RoundOfOrder.java
@@ -1,10 +1,28 @@
 package com.myproject.exercise.utils;
 
+import com.myproject.exercise.App;
+import org.apache.log4j.Logger;
+
 /**
  * Created by skakk2 on 9/10/2016.
  */
 public class RoundOfOrder {
-    public void convertToText(String arg) {
+    static final Logger logger = Logger.getLogger(RoundOfOrder.class);
 
+    public void convertToText(String arg) throws Exception {
+        try{
+            Integer intValueOfInput = Integer.parseInt(arg);
+            if(String.valueOf(intValueOfInput).length() > 8){
+                logger.error("The program cannot take values more than 8 digits");
+                throw new Exception("More than 8 digit values");
+            }else{
+                
+            }
+        } catch (NumberFormatException ne){
+            logger.error("The provided input cannot be convert to an Integer value");
+            throw new NumberFormatException();
+        }
     }
+
+
 }
--- src/test/java/com/myproject/exercise/utils/RoundOfOrderTest.java
+++ src/test/java/com/myproject/exercise/utils/RoundOfOrderTest.java
@@ -1,9 +1,29 @@
 package com.myproject.exercise.utils;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+
 /**
  * Created by skakk2 on 9/10/2016.
  */
 public class RoundOfOrderTest {
+    RoundOfOrder roundOfOrder;
 
+    @Before
+    public void before() {
+        roundOfOrder = new RoundOfOrder();
+    }
 
+    @Test(expected = NumberFormatException.class)
+    public void testCovertForInvalidInput() throws Exception {
+        roundOfOrder.convertToText("abcde");
+    }
+
+    @Test(expected = Exception.class)
+    public void testBiggerIntegerValuesMoreThanEightDigits() throws Exception {
+        String biggerValue = "555555555";
+        roundOfOrder.convertToText(biggerValue);
+    }
 }
